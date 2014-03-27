6d49a51b37e4ba35c1cdd299f0c8d14e341dcbff
Piotr Wilczyński <bevesce@gmail.com>
2014-03-27 18:07:27 +0100

    fixes small bug that crashed Alfred workflow

--- workflows/Alfred/alfredq.py
+++ workflows/Alfred/alfredq.py
@@ -9,4 +9,4 @@ query = ' '.join(sys.argv[1:])
 query = todoflow.expand_query(query)
 
 t = todoflow.from_files(todoflow.lists.to_list()).filter(query)
-XMLPrinter().pprint(t)
+print XMLPrinter().pformat(t)
