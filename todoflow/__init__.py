758320c74379b39dd91f91cb471f7815d54da277
Piotrek Wilczyński <bevesce@gmail.com>
2015-06-17 16:42:23 +0200

    Update __init__.py

--- todoflow/__init__.py
+++ todoflow/__init__.py
@@ -2,4 +2,4 @@ from __future__ import absolute_import
 
 from .todoflow import from_text, from_path, from_paths, from_dir
 from .todoflow import to_path, to_sources
-import textutils
+from . import textutils
